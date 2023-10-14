1789d188af96acf6baed2d2047bb067b18930240
asif987patel <asif987patel@github.com>
2023-10-14 23:21:04 +0530

    fixed: added ShouldQueue interface to listener

--- app/Listeners/PostCreatedListener.php
+++ app/Listeners/PostCreatedListener.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class PostCreatedListener
+class PostCreatedListener implements ShouldQueue
 {
     /**
      * Create the event listener.
