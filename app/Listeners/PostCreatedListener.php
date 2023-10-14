078f10c823b0b623c59b97430c6df8d8ed3e1a32
asif987patel <asif987patel@github.com>
2023-10-14 23:19:27 +0530

    fixed: added ShouldQueue interface to listener

--- app/Listeners/PostCreatedListener.php
+++ app/Listeners/PostCreatedListener.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class PostCreatedListener
+class PostCreatedListener implements ShouldQueue
 {
     use ErrorManager;
     /**
